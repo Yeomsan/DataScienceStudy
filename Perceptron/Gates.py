0e102612236057200857273703355b7746682aeb
Yeomsan <128014856+Yeomsan@users.noreply.github.com>
2024-07-01 07:58:54 +0000

    tmp

--- Perceptron/Gates.py
+++ Perceptron/Gates.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+#Making AND, OR, NAND gates
+
 def AND(x1, x2):
     x = np.array([x1, x2])
     w = np.array([1, 1])
@@ -33,5 +35,17 @@ def NAND(x1, x2):
 """         can use for test
 while True:
     x,y=map(int,input().split())
-    print(x, y, "AND => ", AND(x, y), "OR => ", OR(x, y), "NAND => ", NAND(x, y))
+    print(x, y, " AND => ", AND(x, y), " OR => ", OR(x, y), " NAND => ", NAND(x, y))
+"""
+#Making XOR gate with multi-layer perceptron with AND, OR, NAND
+
+def XOR(x1, x2):
+    s1, s2 = NAND(x1, x2), OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+"""         can use for test
+while True:
+    x,y=map(int,input().split())
+    print(x, y, " XOR => ", XOR(x, y))
 """
