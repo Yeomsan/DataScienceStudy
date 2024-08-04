ea6dc294ef00f45a77ec90e7da816b081ef8c91d
Yeomsan <128014856+Yeomsan@users.noreply.github.com>
2024-08-04 10:33:56 +0000

    from scratch

--- Deep_Learning/Neural_Nets/dataset/mnist.py
+++ Deep_Learning/Neural_Nets/dataset/mnist.py
@@ -127,57 +127,36 @@ def load_mnist(normalize=True, flatten=True, one_hot_label=False):
 if __name__ == '__main__':
     init_mnist()
 
-'''
-Above codes are for download datasets
-and now are for math
-'''
+(x_train, t_train), (x_test, t_test) = \
+    load_mnist(normalize = True, one_hot_label = True)
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+train_size = x_train.shape[0]
+batch_size = 10
+batch_mask = np.random.choice(train_size, batch_size)
+x_batch = x_train[batch_mask]
+t_batch = t_train[batch_mask]
 
-def softmax(a):
-    c = np.max(a)
-    exp_a = np.exp(a-c)
-    sum_exp_a = np.sum(exp_a)
-    y = exp_a / sum_exp_a
+def CEE(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
 
-    return y
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arrange(batch_size), t] + 1e-7)) / batch_size
 
-'''
-math is and end now for nets
-'''
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
 
-def get_data():
-    (x_train, t_train), (x_test, t_test) = \
-        load_mnist(flatten = True, normalize = False, one_hot_label = False)
-    return x_test, t_test
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
 
-def init_network():
-    with open("sample_weight.pkl", 'rb') as f:
-        network = pickle.load(f)
-    return network
+        x[idx] = tmp_val- h
+        fxh2 = f(x)
 
-def predict(network, x):
-    W1, W2, W3 = network['W1'], network['W2'], network['W3']
-    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+        grad[idx] = (fxh1 + fxh2) / (2 * h)
+        x[idx] = tmp_val
 
-    a1 = np.dot(x, W1) + b1
-    z1 = sigmoid(a1)
-    a2 = np.dot(z1, W2) + b2
-    z2 = sigmoid(a2)
-    a3 = np.dot(z2, W3) + b3
-    y = softmax(a3)
-
-    return y
-
-x, t = get_data()
-network = init_network()
-
-accuracy_cnt = 0
-for i in range(len(x)):
-    y = predict(network, x[i])
-    p = np.argmax(y)
-    if p == t[i]:
-        accuracy_cnt += 1
-
-print ("Accuracy:" + str(float(accuracy_cnt)) / len(x))
+    return grad
