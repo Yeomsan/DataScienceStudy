48e6e495040db9005213248d5c1a7a0e3f10e5b4
Yeomsan <128014856+Yeomsan@users.noreply.github.com>
2024-08-06 10:09:37 +0000

    no

--- Deep_Learning/Neural_Nets/mnist.py
+++ Deep_Learning/Neural_Nets/mnist.py
@@ -126,37 +126,3 @@ def load_mnist(normalize=True, flatten=True, one_hot_label=False):
 
 if __name__ == '__main__':
     init_mnist()
-
-(x_train, t_train), (x_test, t_test) = \
-    load_mnist(normalize = True, one_hot_label = True)
-
-train_size = x_train.shape[0]
-batch_size = 10
-batch_mask = np.random.choice(train_size, batch_size)
-x_batch = x_train[batch_mask]
-t_batch = t_train[batch_mask]
-
-def CEE(y, t):
-    if y.ndim == 1:
-        t = t.reshape(1, t.size)
-        y = y.reshape(1, y.size)
-
-    batch_size = y.shape[0]
-    return -np.sum(np.log(y[np.arrange(batch_size), t] + 1e-7)) / batch_size
-
-def numerical_gradient(f, x):
-    h = 1e-4
-    grad = np.zeros_like(x)
-
-    for idx in range(x.size):
-        tmp_val = x[idx]
-        x[idx] = tmp_val + h
-        fxh1 = f(x)
-
-        x[idx] = tmp_val- h
-        fxh2 = f(x)
-
-        grad[idx] = (fxh1 + fxh2) / (2 * h)
-        x[idx] = tmp_val
-
-    return grad
